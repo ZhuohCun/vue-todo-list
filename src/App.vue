cb6354d1b7ebe013189b26c7b3e2ff10407f3691
unknown <zhuohcun@outlook.com>
2024-05-27 17:06:42 +0800

    third

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,18 @@
 </script>
 
 <template>
-
-
-
+  <div class="container"></div>
 </template>
 
 <style scoped>
-
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 80%;
+  margin: 0 auto;
+  background-color: #f3f3f3;
+}
 </style>
