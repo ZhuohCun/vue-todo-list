ba47f6b97f75fb388778139ce9bb270f6cc8c71d
unknown <zhuohcun@outlook.com>
2024-06-17 17:25:31 +0800

    added the edit function

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
 import {ref} from "vue";
+interface ITaskItem{
+  id:number;
+  text:string;
+  isDone:boolean;
+  isEdit:boolean;
+}
 let todoText = ref('')  //'reactive' can also create reactive objects
-let taskList=ref<{id:number,text:String,isDone:boolean}[]>([]);
+let taskList=ref<ITaskItem[]>([]);
+let itemText=ref("")
 taskList.value=JSON.parse(window.localStorage.getItem('taskList')??"[]"); //anti-stringify pattern:a??b meaning that if a is empty,the output string will be replaced with b
 function onAddTask(){
   taskList.value.push(
       {
         id:taskList.value.length+1,  //repetitive issues remain
         text: todoText.value,
-        isDone: false
+        isDone: false,
+        isEdit: false
       }
   );
   todoText.value='';
+  onSave();
+}
+function onSave(){
   window.localStorage.setItem('taskList',JSON.stringify(taskList.value)); //key - value(stringify)
+}
+function onEdit(item:ITaskItem){
+  item.isEdit=true;
+  itemText.value=item.text;
+}
+function onEditOK(item:ITaskItem){
+  item.isEdit=false;
+  item.text=itemText.value;
+  onSave();
 }
 </script>
 
@@ -25,11 +45,19 @@ function onAddTask(){
     </div>
     <div class="task-list">
       <div v-for="item of taskList" class="task-item">
-      <el-checkbox v-model="item.isDone" label="task1" size="large">{{ item.text }}</el-checkbox>
-        <div>
-           <el-button type="primary">Edit</el-button>
-           <el-button type="danger">Delete</el-button>
-        </div>
+        <template v-if="!item.isEdit">
+            <el-checkbox v-model="item.isDone" label="task1" size="large">{{ item.text }}</el-checkbox>
+            <div>
+               <el-button type="primary" @click="onEdit(item)">Edit</el-button>
+               <el-button type="danger">Delete</el-button>
+            </div>
+        </template>
+        <template v-else>
+
+          <el-input v-model="itemText"></el-input>
+          <el-button type="primary" @click="onEditOK(item)">Confirm</el-button>
+          <el-button type="danger" @click="item.isEdit=false">Cancel</el-button>
+        </template>
       </div>
     </div>
   </div>
